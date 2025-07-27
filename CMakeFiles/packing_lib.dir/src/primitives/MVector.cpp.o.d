9a06d202e60f9d93414ad8f2abb2ad3f3b1bd571
misan <misan@disca.upv.es>
2025-07-27 11:57:41 +0000

    Remove binaries and add .gitignore

--- CMakeFiles/packing_lib.dir/src/primitives/MVector.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/packing_lib.dir/src/primitives/MVector.cpp.o: \
- /home/misan/packing2D/src/primitives/MVector.cpp \
- /usr/include/stdc-predef.h \
- /home/misan/packing2D/src/primitives/MVector.h
