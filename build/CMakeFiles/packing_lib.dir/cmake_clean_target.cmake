79085c85b045ec02127d7ff69ee8d1be6dac0cbe
Miguel Sanchez <misan@disca.upv.es>
2025-08-24 22:48:43 +0200

    Delete build directory

--- build/CMakeFiles/packing_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libpacking_lib.a"
-)
--- build/CMakeFiles/packing_lib.dir/src/primitives/MVector.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/packing_lib.dir/src/primitives/MVector.cpp.o: \
-  /Users/misan/Documents/borrame/free_space_Z/src/primitives/MVector.cpp \
-  /Users/misan/Documents/borrame/free_space_Z/src/primitives/MVector.h
